b06623dff69c1fa795b8dde38a729f936d1abcd9
TechPanelGM <giomook2003@gmail.com>
2024-11-01 08:56:39 +0000

    umi: Update Camera Location

--- lineage_umi.mk
+++ lineage_umi.mk
@@ -15,14 +15,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/umi/device.mk)
 
 # Extra Packages
-$(call inherit-product, vendor/xiaomi/camera/miuicamera.mk) # HolyBear Leica camera
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk) # ViPER4AndroidFX
+$(call inherit-product, vendor/xiaomi/miuicamera-sm8250/miuicamera.mk) # HolyBear Leica camera
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)       # ViPER4AndroidFX
 
 # Include Vendor Keys
 include vendor/evolution-priv/keys/keys.mk
 
 # Extra Tags
-EVO_BUILD_TYPE 	        := COMMUNITY
 TARGET_HAS_UDFPS        := true
 WITH_GMS                := true
 BUILD_BCR               := true
